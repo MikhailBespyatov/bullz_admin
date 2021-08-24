cd6e6683f97a4d5c2c8d04c5608a4e199c740edd
Tatyana Lepestova <43875213+Indigoinspn@users.noreply.github.com>
2021-08-24 13:23:25 +0300

    YW-340 fixed model

--- src/constants/services.ts
+++ src/constants/services.ts
@@ -6,5 +6,3 @@ export const baseWomURL = process.env.REACT_APP_WOM_API_URL as string;
 
 export const environmentBASY =
     process.env.REACT_APP_BASY_API_URL === 'https://bullz-dev.bullz.io/docs/index.html' ? 'DEV' : 'PROD';
-export const environmentWOM = process.env.REACT_APP_WOM_API_URL === 'https://wom-dev.xc.io/' ? 'DEV' : 'PROD';
-export const environmentYEAY = process.env.REACT_APP_YEAY_API_URL === 'https://yeay-dev.xc.io/' ? 'DEV' : 'PROD';
