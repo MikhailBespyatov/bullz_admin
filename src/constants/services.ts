14834e071000e2a047b8da9ccca8d203801c7ee5
Tatyana Lepestova <43875213+Indigoinspn@users.noreply.github.com>
2021-08-24 15:09:26 +0300

    YW-340 fixed urls

--- src/constants/services.ts
+++ src/constants/services.ts
@@ -4,5 +4,4 @@ export const nameProject = 'BASY';
 
 export const baseWomURL = process.env.REACT_APP_WOM_API_URL as string;
 
-export const environmentBASY =
-    process.env.REACT_APP_BASY_API_URL === 'https://bullz-dev.bullz.io/docs/index.html' ? 'DEV' : 'PROD';
+export const environmentBASY = process.env.REACT_APP_BASY_API_URL === 'https://bullz-dev.bullz.io/' ? 'DEV' : 'PROD';
