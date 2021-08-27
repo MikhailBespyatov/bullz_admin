bad07b3e19dd7118783e56ff490abba3b8a02a12
Tatyana Lepestova <43875213+Indigoinspn@users.noreply.github.com>
2021-08-27 10:23:28 +0300

    YW-345 returned wom api

--- src/constants/services.ts
+++ src/constants/services.ts
@@ -5,3 +5,4 @@ export const nameProject = 'BASY';
 export const baseWomURL = process.env.REACT_APP_WOM_API_URL as string;
 
 export const environmentBASY = process.env.REACT_APP_BASY_API_URL === 'https://bullz-dev.bullz.io/' ? 'DEV' : 'PROD';
+export const environmentWOM = process.env.REACT_APP_WOM_API_URL === 'https://wom-dev.xc.io/' ? 'DEV' : 'PROD';
