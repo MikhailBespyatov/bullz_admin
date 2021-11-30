e6183393215fa184b81da57b5d1e532c8b91cb32
JuliaMakukhina <miss.makukhina@mail.ru>
2021-11-30 09:56:16 +0000

    YW-622: change background on select and search on page users

--- src/componentsNewDesign/layouts/filterLayouts/UsersFilterLayout/styles.ts
+++ src/componentsNewDesign/layouts/filterLayouts/UsersFilterLayout/styles.ts
@@ -1,4 +1,5 @@
 import { ContentWrapper } from 'componentsNewDesign/wrappers/ContentWrapper';
+import { grey30 } from 'constants/styles/colors';
 import styled from 'styled-components';
 import { IsClosed } from 'types/data';
 
@@ -9,6 +10,10 @@ export const FilterMobileWrapper = styled.div<IsClosed>`
     margin-top: ${({ isClosed }) => (isClosed ? '-180px' : '0')};
     transition-property: margin-top, transform;
     transition-duration: 0.3s, 0.3s;
+
+    @media (max-width: 480px) {
+        margin-bottom: -10px;
+    }
 `;
 
 export const SearchMobileWrapper = styled(ContentWrapper)<IsClosed>`
@@ -17,4 +22,15 @@ export const SearchMobileWrapper = styled(ContentWrapper)<IsClosed>`
     margin-top: ${({ isClosed }) => (isClosed ? '-127px' : '0')};
     transition-property: margin-top, transform;
     transition-duration: 0.3s, 0.3s;
+
+    @media (max-width: 480px) {
+        background-color: ${grey30};
+    }
+`;
+
+export const ContentResetWrapper = styled.div`
+    background-color: ${grey30};
+    display: flex;
+    align-items: center;
+    justify-content: end;
 `;
